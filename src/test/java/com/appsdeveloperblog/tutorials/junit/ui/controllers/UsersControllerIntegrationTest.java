21c907e5d2dd4b504e3c89e9d5eda07989047762
vhumenny <v.humenny@gmail.com>
2023-04-30 11:55:22 +0300

    finished integration tests

--- src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -4,10 +4,7 @@ import com.appsdeveloperblog.tutorials.junit.security.SecurityConstants;
 import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,7 +19,8 @@ import java.util.List;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@TestPropertySource(locations = "/application-test.properties",
 //        properties = "server.port=8081")
-@TestMethodOrder()
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UsersControllerIntegrationTest {
 
     @Value("${server.port}")
@@ -34,8 +32,11 @@ public class UsersControllerIntegrationTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    private String authorizationToken;
+
     @Test
     @DisplayName("User can be created")
+    @Order(1)
     void testCreateUser_whenValidDetailsProvided_returnUserDetails() throws JSONException {
         //Arrange
 
@@ -67,6 +68,7 @@ public class UsersControllerIntegrationTest {
 
     @Test
     @DisplayName("GET /users requires JWT")
+    @Order(2)
     void testGetUsers_whenMissingJWT_returns403() {
         //Arrange
         HttpHeaders headers = new HttpHeaders();
@@ -88,22 +90,48 @@ public class UsersControllerIntegrationTest {
 
     @Test
     @DisplayName("/login works")
+    @Order(3)
     void testUserLogin_whenValidCredentialsProvided_returnsJWTinAuthorizationHeader() throws JSONException {
         //Arrange
         JSONObject loginCredentials = new JSONObject();
-        loginCredentials.put("email", "test3@test.com");
+        loginCredentials.put("email", "test@test.com");
         loginCredentials.put("password", "12345678");
 
         HttpEntity<String> request = new HttpEntity<>(loginCredentials.toString());
         //Act
         ResponseEntity response = testRestTemplate.postForEntity("/users/login", request, null);
 
+        authorizationToken = response.getHeaders().getValuesAsList(SecurityConstants.HEADER_STRING).get(0);
+
         //Assert
         Assertions.assertEquals(HttpStatus.OK, response.getStatusCode(), "Http status code should be 200.");
-        Assertions.assertNotNull(response.getHeaders().getValuesAsList(SecurityConstants.HEADER_STRING).get(0),
-                "Response should contain Authorization header with JWT");
+        Assertions.assertNotNull(authorizationToken, "Response should contain Authorization header with JWT");
         Assertions.assertNotNull(response.getHeaders().getValuesAsList("UserID").get(0),
                 "Response should contain User id in Response header.");
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("GET /users work")
+    void testGetUsers_whenValidJWTProvided_returnsUsers() {
+        //Arrange
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setBearerAuth(authorizationToken);
+
+        HttpEntity request = new HttpEntity(headers);
+        //Act
+        ResponseEntity<List<UserRest>> response = testRestTemplate.exchange("/users",
+                HttpMethod.GET,
+                request,
+                new ParameterizedTypeReference<List<UserRest>>() {
+                });
+
+        //Assert
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode(),
+                "Http status should be 200.");
+        Assertions.assertTrue(response.getBody().size() == 1,
+                "There should be exactly 1 user in the list.");
 
     }
 }
